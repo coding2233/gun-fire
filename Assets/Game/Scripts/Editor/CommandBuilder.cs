58c29c264c9d3e087e68481fa105fd125db9475c
wanderer <dutifulwanderer@gmail.com>
2022-11-16 00:17:42 +0800

    /opt/unity/Editor/Data/il2cpp

--- Assets/Game/Scripts/Editor/CommandBuilder.cs
+++ Assets/Game/Scripts/Editor/CommandBuilder.cs
@@ -17,7 +17,9 @@ public class CommandBuilder
         if (!installController.HasInstalledHybridCLR())
         {
             #if UNITY_EDITOR_LINUX
-                installController.Il2CppInstallDirectory = $"{EditorApplication.applicationPath}/Data/il2cpp";
+                installController.Il2CppInstallDirectory = $"{Path.GetDirectoryName(EditorApplication.applicationPath)}/Data/il2cpp";
+                installController.Il2CppInstallDirectory = installController.Il2CppInstallDirectory.Replace("//", "/");
+                installController.Il2CppInstallDirectory = installController.Il2CppInstallDirectory.Replace("\\", "/");
             #endif
             Debug.Log($"HybridCLR install path for unity linux editor: {installController.Il2CppInstallDirectory}  ==> {installController.Il2CppBranch}");
             installController.InitHybridCLR(installController.Il2CppBranch, installController.Il2CppInstallDirectory);
